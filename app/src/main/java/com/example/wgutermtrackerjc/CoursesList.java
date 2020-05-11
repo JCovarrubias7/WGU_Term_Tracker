4f3ad4bd2e012ab3ec032a986fa6c7be28dcbde6
JCovarrubias7 <artnhector@gmail.com>
2020-05-11 10:51:50 -0500

    Fix menu items.

--- app/src/main/java/com/example/wgutermtrackerjc/CoursesList.java
+++ app/src/main/java/com/example/wgutermtrackerjc/CoursesList.java
@@ -11,9 +11,8 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 
-import com.example.wgutermtrackerjc.data.DBContract;
+import com.example.wgutermtrackerjc.data.DBContract.CourseEntry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -23,7 +22,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class CoursesList extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -54,7 +53,6 @@ public class CoursesList extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(CoursesList.this, AddCourse.class);
-
                 // Get Id from term to pass on to the AddCourse activity
                 intent.putExtra("termId", currentTermId);
                 startActivity(intent);
@@ -78,35 +76,18 @@ public class CoursesList extends AppCompatActivity
         mCursorAdapter = new CourseCursorAdapter(this, null);
         coursesListView.setAdapter(mCursorAdapter);
 
+        // Setup the item click listener on the list items
         coursesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 // Create new intent to go to CourseDetails
                 Intent intent = new Intent(CoursesList.this, CourseDetails.class);
                 // Form the content URI that represents the course that was clicked on
-                Uri currentCourseUri = ContentUris.withAppendedId(DBContract.CourseEntry.CONTENT_URI_COURSES, id);
+                Uri currentCourseUri = ContentUris.withAppendedId(CourseEntry.CONTENT_URI_COURSES, id);
                 // Set the URI on the data fields of the Intent
                 intent.setData(currentCourseUri);
-
-                // Get the TextViews that have the values we want
-                TextView nameView = (TextView) view.findViewById(R.id.list_course_item_name);
-                TextView startView = (TextView) view.findViewById(R.id.list_course_item_start_date);
-                TextView endView = (TextView) view.findViewById(R.id.list_course_item_end_date);
-                TextView statusView = (TextView) view.findViewById(R.id.list_course_item_status_text);
-
-                // Get the string values of each text view of the list
-                String courseName = nameView.getText().toString();
-                String courseStart = startView.getText().toString();
-                String courseEnd = endView.getText().toString();
-                String courseStatus = statusView.getText().toString();
-
-                intent.putExtra("courseId", id);
-                intent.putExtra("courseName", courseName);
-                intent.putExtra("courseStatus", courseStatus);
-                intent.putExtra("courseStart", courseStart);
-                intent.putExtra("courseEnd", courseEnd);
-
-
+                // Get Id from term to pass on to the AddCourse activity
+                intent.putExtra("termId", currentTermId);
                 // Launch the activity to display the data for the current Course
                 startActivity(intent);
             }
@@ -114,14 +95,12 @@ public class CoursesList extends AppCompatActivity
 
         // Initialize the loader to red the term data from the Database
         getLoaderManager().initLoader(EXISTING_TERM_LOADER, null, this);
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_terms.xml file
-        getMenuInflater().inflate(R.menu.menu_term_view, menu);
+        // Inflate the menu options from the res/menu/menu_terms_list.xml file
+        getMenuInflater().inflate(R.menu.menu_courses_list, menu);
         return true;
     }
 
@@ -131,20 +110,8 @@ public class CoursesList extends AppCompatActivity
         // User clicked on a menu option in the app bar overflow menu
         switch (item.getItemId()) {
             // Respond to a click on the "Insert test data" menu option
-            case R.id.action_delete_current_term:
+            case R.id.action_delete_all_courses:
                 showDeleteConfirmationDialog();
-                return true;
-            case R.id.action_edit_current_term:
-                // Get the intent that launched this activity
-                Intent intent = getIntent();
-                // for the content URI that was sent from the TermsList
-                mCurrentTermUri = intent.getData();
-                // Create new intent to go to the AddTerm activity
-                Intent newIntent = new Intent(CoursesList.this, AddTerm.class);
-                // Set the URI on the data files of the Intent
-                newIntent.setData(mCurrentTermUri);
-                // Launch the activity to edit the data for the current term
-                startActivity(newIntent);
                 return true;
             case android.R.id.home:
                 finish();
@@ -158,11 +125,11 @@ public class CoursesList extends AppCompatActivity
         // Create an AlertDialog.Builder and set the message, and click listeners
         // for the positive and negative buttons on the dialog.
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Delete this term?");
+        builder.setMessage("Delete all courses?");
         builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // User clicked the "Delete" button, so delete the product.
-                deleteTerm();
+                deleteCourses();
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -181,14 +148,23 @@ public class CoursesList extends AppCompatActivity
     }
 
     // Perform the deletion of the term in the database
-    private void deleteTerm() {
+    private void deleteCourses() {
         // Only perform the delete if there is an existing term
-        if (mCurrentTermUri != null) {
+        if (CourseEntry.CONTENT_URI_COURSES != null) {
             // Call the ContentResolver to delete the product at the given URI.
-            int rowsDeleted = getContentResolver().delete(mCurrentTermUri, null, null);
+            int rowsDeleted = getContentResolver().delete(CourseEntry.CONTENT_URI_COURSES, null, null);
+            // Show a toast message depending on condition
+            if (rowsDeleted == 0) {
+                // If no rows were deleted, then there was an error with the delete.
+                Toast.makeText(this, "Error with deleting all Courses",
+                        Toast.LENGTH_SHORT).show();
+            }
+            else {
+                // Otherwise, the delete was successful and we can display a toast.
+                Toast.makeText(this, "All courses successfully deleted",
+                        Toast.LENGTH_SHORT).show();
+            }
         }
-        // Close the activity
-        finish();
     }
 
     @Override
@@ -196,20 +172,20 @@ public class CoursesList extends AppCompatActivity
         // Get the columns from the table using the URI.
         // Define a projection that specifies the columns from the table we care about
         String[] projection = {
-                DBContract.CourseEntry._ID,
-                DBContract.CourseEntry.COLUMN_ASSOCIATED_TERM_ID,
-                DBContract.CourseEntry.COLUMN_COURSE_NAME,
-                DBContract.CourseEntry.COLUMN_COURSE_START,
-                DBContract.CourseEntry.COLUMN_COURSE_END,
-                DBContract.CourseEntry.COLUMN_COURSE_STATUS};
+                CourseEntry._ID,
+                CourseEntry.COLUMN_ASSOCIATED_TERM_ID,
+                CourseEntry.COLUMN_COURSE_NAME,
+                CourseEntry.COLUMN_COURSE_START,
+                CourseEntry.COLUMN_COURSE_END,
+                CourseEntry.COLUMN_COURSE_STATUS};
 
         // Define a selection
-        String selection = DBContract.CourseEntry.COLUMN_ASSOCIATED_TERM_ID + "=?";
+        String selection = CourseEntry.COLUMN_ASSOCIATED_TERM_ID + "=?";
         String[] selectionArgs = {String.valueOf(currentTermId)};
 
         // This loader will execute the ContentProvider's query method on a background thread
         return new CursorLoader(this,
-                DBContract.CourseEntry.CONTENT_URI_COURSES,
+                CourseEntry.CONTENT_URI_COURSES,
                 projection,
                 selection,
                 selectionArgs,
