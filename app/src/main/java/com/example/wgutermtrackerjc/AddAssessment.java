c81f91f559b9ff5b6f00eb975043b25771f4c1d9
JCovarrubias7 <artnhector@gmail.com>
2020-05-22 00:26:26 -0500

    Change menu up action behavior.

--- app/src/main/java/com/example/wgutermtrackerjc/AddAssessment.java
+++ app/src/main/java/com/example/wgutermtrackerjc/AddAssessment.java
@@ -181,7 +181,7 @@ public class AddAssessment extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+            onBackPressed();
             return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/wgutermtrackerjc/AddTerm.java
+++ app/src/main/java/com/example/wgutermtrackerjc/AddTerm.java
@@ -181,7 +181,7 @@ public class AddTerm extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+            onBackPressed();
             return true;
         }
         return super.onOptionsItemSelected(item);
